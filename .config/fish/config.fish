d6f9f7d8ddddcbd6e5fe81a650f718e64e1db500
Carlos Lespin <carlos.lespin.silva@gmail.com>
2025-03-05 21:52:07 -0600

    feat: ghostty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,16 @@
 if status is-interactive
     set -g fish_greeting ""
+    alias size="ncdu"
     alias open="explorer.exe ."
     alias ls="exa --icons --group-directories-first --long --no-user --no-permissions --no-time"
     alias power="pwsh.exe"
     alias cd="z"
+
+    set -x EDITOR nvim
+    set -x VISUAL nvim
+    set -x PAGER bat
+    set -x LESS -R
+
     starship init fish | source
     zoxide init fish | source
 end
